558533e7e4450785011a83fd6f0447134a71d94d
Maxim Panin <panin148893@gmail.com>
2025-05-31 18:00:50 +0300

    1-st commit

--- src/main/java/org/example/lesson2_1/Main.java
+++ src/main/java/org/example/lesson2_1/Main.java
@@ -2,6 +2,6 @@ package org.example.lesson2_1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Aston!");
+        System.out.println("Hello Aston !");
     }
 }
