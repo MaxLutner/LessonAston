c3e0ff6ab07c3e5589dcbc9663641d5d563659a0
Maxim Panin <panin148893@gmail.com>
2025-06-07 18:57:11 +0300

    5-th lesson

--- src/main/java/org/example/lesson2_4/case1/Bowl.java
+++ src/main/java/org/example/lesson2_4/case1/Bowl.java
@@ -2,9 +2,11 @@ package org.example.lesson2_4.case1;
 
 public class Bowl {
     int amountOfFood;
+
     void addFood(int amountOfFood){
         this.amountOfFood+=amountOfFood;
     }
+
     void checkFood(){
         System.out.println("еды в миске:" + amountOfFood);
     }
--- src/main/java/org/example/lesson2_4/case1/Main.java
+++ src/main/java/org/example/lesson2_4/case1/Main.java
@@ -5,14 +5,18 @@ public class Main {
         Animal a = new Dog();
         Animal b = new Cat();
         Animal c = new Cat();
+
         System.out.println("Общее количество животных: " + Animal.getCountAnimals());
         System.out.println("Количество собак: " + Dog.getCountDogs());
         System.out.println("Количество котов: " + Cat.getCountCats());
+
         Cat cats[]=new Cat[5];
+
         for (int i = 0; i < cats.length; i++) {
             cats[i] = new Cat();
         }
         Bowl bowl = new Bowl();
+
         bowl.addFood(100);
         for (int i = 0; i < cats.length; i++) {
             bowl.checkFood();
